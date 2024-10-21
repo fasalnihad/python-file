30426b4a452f8f10fdab4ae205e09fbb0f6873d2
fasalnihad <fasalnihad12@gmail.com>
2024-10-21 14:09:51 +0530

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,30 +1,30 @@
-# class Solution:
-#     def longestPalindrome(self, s: str) -> str:
-#         # Helper function to expand around the center
-#         def expand_around_center(left: int, right: int) -> str:
-#             while left >= 0 and right < len(s) and s[left] == s[right]:
-#                 left -= 1
-#                 right += 1
-#             # return the palindrome found by expanding around the center
-#             return s[left + 1:right]
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # Helper function to expand around the center
+        def expand_around_center(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            # return the palindrome found by expanding around the center
+            return s[left + 1:right]
         
-#         # If the string is empty or has 1 character, it's already the longest palindrome
-#         if len(s) <= 1:
-#             return s
+        # If the string is empty or has 1 character, it's already the longest palindrome
+        if len(s) <= 1:
+            return s
         
-#         longest_palindrome = ""
+        longest_palindrome = ""
         
-#         for i in range(len(s)):
-#             # Odd length palindromes (single character center)
-#             palindrome1 = expand_around_center(i, i)
+        for i in range(len(s)):
+            # Odd length palindromes (single character center)
+            palindrome1 = expand_around_center(i, i)
             
-#             # Even length palindromes (two character center)
-#             palindrome2 = expand_around_center(i, i + 1)
+            # Even length palindromes (two character center)
+            palindrome2 = expand_around_center(i, i + 1)
             
-#             # Update the longest palindrome if a longer one is found
-#             longest_palindrome = max(longest_palindrome, palindrome1, palindrome2, key=len)
+            # Update the longest palindrome if a longer one is found
+            longest_palindrome = max(longest_palindrome, palindrome1, palindrome2, key=len)
         
-#         return longest_palindrome
+        return longest_palindrome
 
 
 # def longest_palindromic_substring(self,s: str):
